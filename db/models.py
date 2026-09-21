b8d0b1f55679f353df9b6f963e35231cb5a9acb1
mintrecc <maxdavydenkooo@gmail.com>
2026-09-21 10:50:37 +0300

    fixed small bugs

--- db/models.py
+++ db/models.py
@@ -98,7 +98,7 @@ class Ticket(models.Model):
     def __str__(self) -> str:
         return (f"{self.movie_session.movie.title} "
                 f"{self.movie_session.show_time} "
-                f"(row: {self.row}, seat: {self.seat}")
+                f"(row: {self.row}, seat: {self.seat})")
 
     def clean(self) -> None:
         rows = self.movie_session.cinema_hall.rows
