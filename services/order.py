887cf6147edf209fcddbbb159741ecd96a6e2de3
mintrecc <maxdavydenkooo@gmail.com>
2026-09-21 09:49:59 +0300

    fixed return in functions

--- services/order.py
+++ services/order.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.db.models import QuerySet
 
-from db.models import Ticket, Order, User
+from db.models import Ticket, Order
 
 
 @transaction.atomic
@@ -12,7 +12,7 @@ def create_order(
         tickets: list[dict],
         username: str,
         date: datetime.datetime = None,
-) -> User:
+) -> Order:
 
     user = get_user_model().objects.get(username=username)
     order = Order.objects.create(user=user)
@@ -34,7 +34,7 @@ def create_order(
 
 def get_orders(
         username: str = None
-) -> QuerySet:
+) -> QuerySet[Order]:
     if username:
         return Order.objects.filter(user__username=username)
     return Order.objects.all()
--- services/user.py
+++ services/user.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+
 from db.models import User
 
 
@@ -7,8 +9,8 @@ def create_user(
         email: str = None,
         first_name: str = None,
         last_name: str = None,
-) -> None:
-    user = User.objects.create_user(
+) -> User:
+    user = get_user_model().objects.create_user(
         username=username,
         password=password,
     )
@@ -23,7 +25,7 @@ def create_user(
 
 
 def get_user(user_id: int) -> User:
-    return User.objects.get(id=user_id)
+    return get_user_model().objects.get(id=user_id)
 
 
 def update_user(
@@ -33,8 +35,8 @@ def update_user(
         email: str = None,
         first_name: str = None,
         last_name: str = None,
-) -> None:
-    user = User.objects.get(id=user_id)
+) -> User:
+    user = get_user_model().objects.get(id=user_id)
     if username:
         user.username = username
     if password:
